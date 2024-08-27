87a0d02d68099a45a11e58c8d8cd395a4d6d0b2e
fiona <fiona.mburu@student.moringaschool.com>
2024-08-27 15:28:03 +0300

    initial comment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()  
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Fiona")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+greet_with_default()
+
+def add(num1=45, num2=55):
+    return num1 + num2
+
+add()
+
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(10)
